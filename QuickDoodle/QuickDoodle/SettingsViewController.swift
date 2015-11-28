ca23c47ab65739f48cbb3d9e83292a97219307d0
efreestone <efreestone@fullsail.edu>
2015-11-28 14:25:53 -0700

    Initial commit for Quick Doodle with rebuilt starter project from Ray Wenderlich tut.

---
+++ QuickDoodle/QuickDoodle/SettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewController.swift
+//  QuickDoodle
+//
+//  Created by Elijah Freestone on 11/28/15.
+//  Copyright © 2015 Elijah Freestone. All rights reserved.
+//
+
+import Foundation
