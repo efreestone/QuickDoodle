c234eae9e29e6c60d1cd2b4da37decb445032b22
efreestone <efreestone@fullsail.edu>
2016-04-07 17:27:35 -0600

    Four sample color buttons are hooked up and working correctly. WIll probably add a couple more but not sure what yet

--- QuickDoodle/QuickDoodle/RGBInfoViewController.swift
+++ QuickDoodle/QuickDoodle/RGBInfoViewController.swift
@@ -26,9 +26,9 @@ class RGBInfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redFloat = 150.0 / 255
+//        redFloat = 150.0 / 255
         
-        print("Red = \(redFloat * 255),\nGreen = \(greenFloat * 255),\nBlue = \(blueFloat * 255)")
+//        print("Red = \(redFloat * 255),\nGreen = \(greenFloat * 255),\nBlue = \(blueFloat * 255)")
 
         // Do any additional setup after loading the view.
     }
@@ -36,6 +36,44 @@ class RGBInfoViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Color example button clicked, set RGB floats accordingly
+    @IBAction func colorButtonClicked(sender: UIButton) {
+        let buttonClicked = sender
+        
+        //Brown selected
+        if buttonClicked.tag == 1 {
+            redFloat = 160.0 / 255
+            greenFloat = 82.0 / 255
+            blueFloat = 45.0 / 255
+            print("Brown clicked")
+        }
+        
+        //Sand selected
+        if buttonClicked.tag == 2 {
+            redFloat = 222.0 / 255
+            greenFloat = 217.0 / 255
+            blueFloat = 184.0 / 255
+            print("Sand clicked")
+        }
+        
+        //Hot Pink selected
+        if buttonClicked.tag == 3 {
+            redFloat = 1.0
+            greenFloat = 50.0 / 255
+            blueFloat = 1.0
+            print("Hot Pink clicked")
+        }
+        
+        //Dark Blue selected
+        if buttonClicked.tag == 4 {
+            redFloat = 0.0
+            greenFloat = 0.0
+            blueFloat = 140.0 / 255
+            print("Dark Blue clicked")
+        }
+        print("Red = \(redFloat * 255),\nGreen = \(greenFloat * 255),\nBlue = \(blueFloat * 255)")
     }
     
     //Close button clicked, dismiss view and notify delegate
